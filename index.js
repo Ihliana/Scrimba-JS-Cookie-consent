346def7468b60f3a9d5c467b81481be181eb7b22
Ihliana <Ihnatco@protonmail.com>
2023-08-18 10:08:06 +0200

    Close the modal
    Use an event listener to set the display
    property of the modal to 'none' when the close button is clicked.

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ Challenge:
 const modal = document.getElementById("modal")
 
 setTimeout(() => modal.style.display = "inline", 1500)
+
+/*
+Challenge 2:
+1. Take control of the close button.
+2. Use an event listener to set the display 
+   property of the modal to 'none' when the
+   close button is clicked.
+*/
+
+const closeModal = document.getElementById("modal-close-btn")
+closeModal.addEventListener('click', function(){
+   modal.style.display = "none"
+})
