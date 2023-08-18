9bedfae0ee9258a8ccde7cfd79bf47d4c0c81813
Ihliana <Ihnatco@protonmail.com>
2023-08-18 12:37:09 +0200

    buttons in forms
    buttons have weird behaviors in forms...we use 'type=button' to stop this behavior

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-/*
-Aside Challenge:
-1. Log out the phrase "Modal Opened!" 
-   after a 1.5 second delay.
-*/
-
-/*
-Challenge:
-1. Take control of the 'modal' div with JavaScript.
-2. Swap out our console.log for a line of code
-   which will change the CSS 'display' property
-   of our modal to 'inline'.
-*/
-
 const modal = document.getElementById("modal")
-
-setTimeout(() => modal.style.display = "inline", 1500)
-
-/*
-Challenge 2:
-1. Take control of the close button.
-2. Use an event listener to set the display 
-   property of the modal to 'none' when the
-   close button is clicked.
-*/
-
 const modalCloseModal = document.getElementById("modal-close-btn")
 modalCloseModal.addEventListener('click', function(){
    modal.style.display = "none"
+})
+
+setTimeout(function(){
+   modal.style.display = 'inline'
+}, 1500)
+
+
+/*
+Challenge:
+1. Take control of the form element. 
+2. Add an eventListener to the form to listen for a
+   "submit" event.
+3. When a user clicks "accept", prevent the default
+   behaviour that triggers the refresh.
+4. Log out "form submitted".
+*/  
+
+
+
+const consentForm = document.getElementById("consent-form")
+consentForm.addEventListener("submit", (e) => {
+   e.preventDefault()
+   console.log('form submitted')
 })
