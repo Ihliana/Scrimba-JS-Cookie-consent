d6bcf7511f444c87e9908a9cd538daeedc2a60d9
Ihliana <Ihnatco@protonmail.com>
2023-08-20 09:31:17 +0200

    Add modal message
    changing the modal content after hitting 'submit' button

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const modal = document.getElementById("modal")
 const modalCloseModal = document.getElementById("modal-close-btn")
+const consentForm = document.getElementById("consent-form")
+
+const modalText = document.getElementById('modal-text')
+
+
 modalCloseModal.addEventListener('click', function(){
    modal.style.display = "none"
 })
@@ -8,21 +13,67 @@ setTimeout(function(){
    modal.style.display = 'inline'
 }, 1500)
 
+consentForm.addEventListener("submit", (e) => {
+   e.preventDefault()
+   
+   modalText.innerHTML = `
+         <div class="modal-inner-loading">
+            <img src="images/loading.svg" class="loading">
+            <p id="upload-text">
+            Uploading your data to the dark web...
+            </p>
+         </div>`
+
+   setTimeout(function(){
+            document.getElementById('upload-text').textContent = "Making the sale..."
+     },1500)
+
+   setTimeout(function(){
+      document.getElementById('modal-inner').innerHTML = `
+         <h2>Thanks you sucker! </h2>
+            <p>We just sold the rights to your eternal soul.</p>
+            <div class="idiot-gif">
+               <img src="images/pirate.gif">
+            </div>
+      ` 
+   }, 3000)
+})
+
 
 /*
 Challenge:
-1. Take control of the form element. 
-2. Add an eventListener to the form to listen for a
-   "submit" event.
-3. When a user clicks "accept", prevent the default
-   behaviour that triggers the refresh.
-4. Log out "form submitted".
+1. Take control of the "modal-text" element. 
+2. Make it so that when a user clicks on 
+the accept button, the HTML string below
+is inserted into the modal-text div.
+
+<div class="modal-inner-loading">
+<img src="images/loading.svg" class="loading">
+<p id="uploadText">
+Uploading your data to the dark web...
+</p>
+</div>
+*/
+
+/*   
+Challenge: 
+1. Use a setTimeout to make the phrase "Uploading
+   your data to the dark web" change to "Making the 
+   sale..." after 1.5 seconds.
+⚠️ Do not change the loading svg!
 */  
 
-
-
-const consentForm = document.getElementById("consent-form")
-consentForm.addEventListener("submit", (e) => {
-   e.preventDefault()
-   console.log('form submitted')
-})
+/*   
+Challenge: 
+1. Make it so that 1.5 seconds after seeing the 
+   "Making the sale..." message, the modal is 
+   cleared of its content and the following 
+   string of HTML is displayed instead.
+   
+   `<h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>
+    ` 
+*/  
